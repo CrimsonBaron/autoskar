67329f8b69ef2dfdb0a7b4c18162efe4aa6f15e2
Adam Kišš <contact.adam.kiss@gmail.com>
2025-02-14 20:45:15 +0100

    added caroulsesl functionalyty

--- app/components/ui/carousel.tsx
+++ app/components/ui/carousel.tsx
@@ -1,14 +1,200 @@
-import React, { type JSX } from "react";
+"use client";
+import React, {
+  useEffect,
+  useRef,
+  useState,
+  createContext,
+  useContext,
+  type JSX,
+} from "react";
+
+import { motion } from "framer-motion";
+import { cn } from "~/lib/utils";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 interface CarouselProps {
-    items: JSX.Element[];
-    initialsScroll?: number;
-
+  items: JSX.Element[];
+  initialScroll?: number;
 }
 
-//TODO: Implement carousel
-export function Carousel(){
-    return (
-        <div></div>
-    )
-}
+type Card = {
+  src: string;
+  title: string;
+  description: string;
+};
+
+export const CarouselContext = createContext<{
+  onCardClose: (index: number) => void;
+  currentIndex: number;
+}>({
+  onCardClose: () => {},
+  currentIndex: 0,
+});
+
+export const Carousel = ({ items, initialScroll = 0 }: CarouselProps) => {
+  const carouselRef = React.useRef<HTMLDivElement>(null);
+  const [canScrollLeft, setCanScrollLeft] = React.useState(false);
+  const [canScrollRight, setCanScrollRight] = React.useState(true);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollLeft = initialScroll;
+      checkScrollability();
+    }
+  }, [initialScroll]);
+
+  const checkScrollability = () => {
+    if (carouselRef.current) {
+      const { scrollLeft, scrollWidth, clientWidth } = carouselRef.current;
+      setCanScrollLeft(scrollLeft > 0);
+      setCanScrollRight(scrollLeft < scrollWidth - clientWidth);
+    }
+  };
+
+  const scrollLeft = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({ left: -300, behavior: "smooth" });
+    }
+  };
+
+  const scrollRight = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({ left: 300, behavior: "smooth" });
+    }
+  };
+
+
+  const isMobile = () => {
+    return window && window.innerWidth < 768;
+  };
+
+  return (
+    <CarouselContext.Provider
+      value={{ onCardClose: (index: number) => setCurrentIndex(index), currentIndex }}
+    >
+      <div className="relative w-full">
+        <div
+          className="flex w-full overflow-x-scroll overscroll-x-auto py-10 md:py-10 scroll-smooth [scrollbar-width:none]"
+          ref={carouselRef}
+          onScroll={checkScrollability}
+        >
+          <div
+            className={cn(
+              "absolute right-0  z-[1000] h-auto  w-[5%] overflow-hidden bg-gradient-to-l"
+            )}
+          ></div>
+
+          <div
+            className={cn(
+              "flex flex-row justify-start gap-4 pl-4",
+              "max-w-7xl mx-auto" // remove max-w-4xl if you want the carousel to span the full width of its container
+            )}
+          >
+            {items.map((item, index) => (
+              <motion.div
+                initial={{
+                  opacity: 0,
+                  y: 20,
+                }}
+                animate={{
+                  opacity: 1,
+                  y: 0,
+                  transition: {
+                    duration: 0.5,
+                    delay: 0.2 * index,
+                    ease: "easeOut",
+                    once: true,
+                  },
+                }}
+                key={"card" + index}
+                className="last:pr-[5%] md:last:pr-[33%]  rounded-3xl"
+              >
+                {item}
+              </motion.div>
+            ))}
+          </div>
+        </div>
+        <div className="flex justify-start gap-2 mr-10">
+          <button
+            className="relative z-40 h-15 w-15 rounded-full bg-gray-100 flex items-center justify-center disabled:opacity-50"
+            onClick={scrollLeft}
+            disabled={!canScrollLeft}
+          >
+            <ArrowLeft className="h-6 w-6 text-gray-500"/>
+          </button>
+          <button
+            className="relative z-40 h-15 w-15 rounded-full bg-gray-100 flex items-center justify-center disabled:opacity-50"
+            onClick={scrollRight}
+            disabled={!canScrollRight}
+          >
+            <ArrowRight className="h-6 w-6 text-gray-500" />
+          </button>
+        </div>
+      </div>
+    </CarouselContext.Provider>
+  );
+};
+
+export const Card = ({
+  card,
+  index,
+  layout = false,
+}: {
+  card: Card;
+  index: number;
+  layout?: boolean;
+}) => {
+  const [open, setOpen] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { onCardClose, currentIndex } = useContext(CarouselContext);
+
+  useEffect(() => {
+    function onKeyDown(event: KeyboardEvent) {
+      if (event.key === "Escape") {
+        handleClose();
+      }
+    }
+
+    if (open) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    window.addEventListener("keydown", onKeyDown);
+    return () => window.removeEventListener("keydown", onKeyDown);
+  }, [open]);
+
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    onCardClose(index);
+  };
+
+  return (
+    <>
+      <motion.div
+        layoutId={layout ? `card-${card.title}` : undefined}
+        onClick={handleOpen}
+        className="rounded-3x w-56 md:w-64 overflow-hidden flex flex-col items-center justify-center relative z-1"
+      >
+        <img
+          src={card.src}
+          alt={card.title}
+          className="w-56 h-56 object-cover rounded-3xl"
+        />
+        <div className="p-4 flex flex-col gap-2">
+          <h3 className="text-lg font-bold text-gray-500">{card.title}</h3>
+          <span className="text-xs text-gray-500">{card.description}</span>
+        </div>
+      </motion.div>
+    </>
+  );
+};
+
+
--- app/home/home-page.tsx
+++ app/home/home-page.tsx
@@ -1,6 +1,48 @@
+import { Card, Carousel } from "~/components/ui/carousel";
 import { DefaultLayout } from "~/layout/default-layout";
 
+const data = [
+  {
+    description: "Artificial Intelligence",
+    title: "You can do more with AI.",
+    src: "https://images.unsplash.com/photo-1593508512255-86ab42a8e620?q=80&w=3556&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    description: "Productivity",
+    title: "Enhance your productivity.",
+    src: "https://images.unsplash.com/photo-1531554694128-c4c6665f59c2?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    description: "Product",
+    title: "Launching the new Apple Vision Pro.",
+    src: "https://images.unsplash.com/photo-1713869791518-a770879e60dc?q=80&w=2333&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+ 
+  {
+    description: "Product",
+    title: "Maps for your iPhone 15 Pro Max.",
+    src: "https://images.unsplash.com/photo-1599202860130-f600f4948364?q=80&w=2515&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    description: "iOS",
+    title: "Photography just got better.",
+    src: "https://images.unsplash.com/photo-1602081957921-9137a5d6eaee?q=80&w=2793&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    description: "Hiring",
+    title: "Hiring for a Staff Software Engineer",
+    src: "https://images.unsplash.com/photo-1511984804822-e16ba72f5848?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
+
 export function HomePage() {
+  const cards = data.map((card, index) => (
+    <Card key={card.src} card={card} index={index} />
+  ));
+
+
+
   return (
     <DefaultLayout>
       <div className="flex sm:flex-row flex-col md:p-15">
@@ -50,7 +92,7 @@ export function HomePage() {
             A to není vše!
         </h1>
         <div className="md:p-10">
-
+          <Carousel items={cards} />
         </div>
       </div>
     </DefaultLayout>
