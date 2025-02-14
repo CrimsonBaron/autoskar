3678b5cc8aeb37f1e159f0517bfbc08872cf64dc
Adam Kišš <contact.adam.kiss@gmail.com>
2025-02-14 21:32:12 +0100

    small updates that went boom

--- app/home/home-page.tsx
+++ app/home/home-page.tsx
@@ -1,4 +1,7 @@
+import { Scroll } from "lucide-react";
 import { Card, Carousel } from "~/components/ui/carousel";
+import PricingCalculator from "~/components/ui/pricing-calculator";
+import { ScrollingImages } from "~/components/ui/scrolling-images";
 import { DefaultLayout } from "~/layout/default-layout";
 
 const data = [
@@ -34,6 +37,15 @@ const data = [
     src: "https://images.unsplash.com/photo-1511984804822-e16ba72f5848?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
+
+const images = [
+  "https://images.unsplash.com/photo-1511984804822-e16ba72f5848?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1602081957921-9137a5d6eaee?q=80&w=2793&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1599202860130-f600f4948364?q=80&w=2515&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1713869791518-a770879e60dc?q=80&w=2333&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1531554694128-c4c6665f59c2?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1593508512255-86ab42a8e620?q=80&w=3556&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+]
 
 
 export function HomePage() {
@@ -91,8 +103,24 @@ export function HomePage() {
         <h1 className="text-5xl font-medium leading-tight tracking-wide flex-1  text-center">
             A to není vše!
         </h1>
-        <div className="md:p-10">
+        <div className="md:p-5">
           <Carousel items={cards} />
+        </div>
+      </div>
+      <div className="md:p-10 py-6">
+        <h1 className="text-5xl font-medium leading-tight tracking-wide flex-1  text-center">
+          Nahlédněte do naší dílny 
+        </h1>
+        <div className="md:p-20 mt-10 bg-white rounded-3xl overflow-hidden">
+          <ScrollingImages images={images} />
+        </div>
+      </div>
+      <div className="md:p-10 py-6">
+        <h1 className="text-5xl font-medium leading-tight tracking-wide flex-1  text-center">
+          Cenová kalkulace
+        </h1>
+        <div className="md:p-20 mt-10 bg-white rounded-3xl">
+          <PricingCalculator />
         </div>
       </div>
     </DefaultLayout>
